81ee4156722d06ce5ab54f809cd8b62155225da1
Manik <maniksingh10@gmail.com>
2018-10-01 18:35:49 +0530

    HideStatus

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import './UI/dHome.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  
   runApp(MyApp());
+
 }
 
 class MyApp extends StatelessWidget{
+  
   @override
     Widget build(BuildContext context) {
       // TODO: implement build
+      SystemChrome.setEnabledSystemUIOverlays([]);
       return MaterialApp(
         title: "fLogin",
         home: Home(),
